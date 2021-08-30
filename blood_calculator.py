ea8db0a4e183404fe7ee9e6b8d0e0c193315c941
Ramona Naseri <rsn6@duke.edu>
2021-08-30 13:04:25 -0400

    Added HDL interface

--- blood_calculator.py
+++ blood_calculator.py
@@ -3,14 +3,24 @@ def interface():
     keep_running = True
     while keep_running:
         print("Make a choice")
+        print("1 - HDL Analysis")
         print("9 - Quit")
         choice = int(input("Make a choice: "))
         print(type(choice))
         if choice == 9:
             keep_running = False
+        elif choice == 1:
+            HDL_Driver()
 
 
     print(choice)
     return choice
 
-interface()
+def HDL_Driver(): 
+    hdl_input()
+    hdl_analysis()
+    hdl_output()
+
+def hdl_input():
+    hdl_value = int(input(("Enter HDL Value: "))
+    return hdl_value
